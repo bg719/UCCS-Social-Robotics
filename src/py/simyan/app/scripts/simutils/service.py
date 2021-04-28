5dada925e267260615c3a68c71e1e465e0e900e9
ancient-sentinel <57508187+ancient-sentinel@users.noreply.github.com>
2021-04-27 18:59:59 -0600

    code(py): allow parameters to be passed to service scope

--- src/py/simyan/app/scripts/simutils/service.py
+++ src/py/simyan/app/scripts/simutils/service.py
@@ -3,27 +3,42 @@ __author__ = 'ancient-sentinel'
 
 import qi
 
+from inspect import  isclass
+
 
 class ServiceScope:
     """The scope for a service registered within the context of an existing Application"""
 
     def __init__(self, qiapp, service_class, service_name=None):
+        if not isclass(service_class):
+            raise ValueError('The service must be a class.')
+
         self.service_class = service_class
         if not service_name:
             service_name = service_class.__name__
         self.name = service_name
-        self.instance = service_class(qiapp)
-        self.id = None
+
+        self.qiapp = qiapp
         self.session = qiapp.session
-        self.is_started = False
         self.err_msg = None
 
-    def create_scope(self):
-        """Creates an instance of the service and registers it with the application session."""
-        if self.is_started:
+        self._id = None
+        self._instance = None
+        self._is_started = False
+
+    def create_scope(self, *args, **kwargs):
+        """
+        Creates an instance of the service and registers it with the application session.
+
+        :param args: (Any) The positional arguments to pass to the instance.
+        :param kwargs: (Any) The keyword arguments to pass to the instance.
+        """
+        if self._is_started:
             return
 
-        self.id = self.session.registerService(self.name, self.instance)
+        self._instance = self.service_class(self.qiapp, *args, **kwargs)
+
+        self._id = self.session.registerService(self.name, self.instance)
 
         if hasattr(self.instance, "on_start"):
             def handle_on_start_done(on_start_future):
@@ -41,7 +56,7 @@ class ServiceScope:
 
             qi.async(self.instance.on_start).addCallback(handle_on_start_done)
 
-        self.is_started = self.id is not None
+        self._is_started = self.id is not None
 
     def close_scope(self):
         """Unregisters the service instance from the application session and disposes the instance."""
@@ -53,7 +68,19 @@ class ServiceScope:
                 qi.async(self.instance.on_stop).wait()
 
             self.session.unregisterService(self.id)
-            self.id = None
-            self.is_started = False
-            self.instance = None
+            self._id = None
+            self._instance = None
+            self._is_started = False
+
+    def id(self):
+        """Gets the ID of the service."""
+        return self._id
+
+    def instance(self):
+        """Gets the service instance."""
+        return self._instance
+
+    def is_started(self):
+        """Gets a value indicating whether the service is started."""
+        return self._is_started
 
