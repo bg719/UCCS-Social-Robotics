6a266ce0d4287d8014f7eaea71bcfc3dd2bbe06e
ancient-sentinel <57508187+ancient-sentinel@users.noreply.github.com>
2021-04-28 21:16:20 -0600

    fix(py): modifications to ServiceScope

--- src/py/simyan/app/scripts/simutils/service.py
+++ src/py/simyan/app/scripts/simutils/service.py
@@ -8,14 +8,22 @@ class ServiceScope:
     """The scope for a service registered within the context of an existing Application"""
 
     def __init__(self, qiapp, service_class, service_name=None):
+        """
+        Initializes a new service scope object.
+
+        :param qiapp: (qi.Application) The hosting qi application.
+        :param service_class: (class) The service class.
+        :param service_name: (str) A name for the service. If None, the
+            class name will be used by default.
+        """
         self.service_class = service_class
         if not service_name:
             service_name = service_class.__name__
         self.name = service_name
-        self.instance = service_class(qiapp)
-        self.id = None
-        self.session = qiapp.session
-        self.is_started = False
+        self.instance = None
+        self._id = None
+        self._qiapp = qiapp
+        self._session = qiapp.session
         self.err_msg = None
 
     def create_scope(self):
@@ -23,7 +31,8 @@ class ServiceScope:
         if self.is_started:
             return
 
-        self.id = self.session.registerService(self.name, self.instance)
+        self.instance = self.service_class(self._qiapp)
+        self._id = self._session.registerService(self.name, self.instance)
 
         if hasattr(self.instance, "on_start"):
             def handle_on_start_done(on_start_future):
@@ -41,19 +50,21 @@ class ServiceScope:
 
             qi.async(self.instance.on_start).addCallback(handle_on_start_done)
 
-        self.is_started = self.id is not None
-
     def close_scope(self):
         """Unregisters the service instance from the application session and disposes the instance."""
-        if self.id:
+        if self._id:
             # Call the services on_stop function if available
             if hasattr(self.instance, "on_stop"):
                 # We need a qi.async call so that if the class is single threaded,
                 # it will wait for callbacks to be finished.
                 qi.async(self.instance.on_stop).wait()
 
-            self.session.unregisterService(self.id)
-            self.id = None
-            self.is_started = False
+            self._session.unregisterService(self._id)
+            self._id = None
             self.instance = None
+
+    @property
+    def is_started(self):
+        """Returns a value indication whether a scope has been created and the service started."""
+        return self._id is not None
 
